388aef95c4e4e9df40fbbf53cd2b8f5b3f0f89c2
Mika88 <mikaspivak@gmail.com>
2018-12-31 22:17:09 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,7 +16,7 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.create(params)
-    redirect_to recipe_path(@recipe)
-    
+    redirect_to recipes_path
+
   end
 end
